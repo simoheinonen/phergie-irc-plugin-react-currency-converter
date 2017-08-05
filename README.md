3771f75bb74be9abcc1683f5572c48ecc1dd9e19
simoheinonen <simo@dudgeon.fi>
2017-08-05 16:11:08 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# Currency converter plugin for [Phergie](http://github.com/phergie/phergie-irc-bot-react/)
+
+## Installation
+
+```
+composer require simoheinonen/phergie-irc-plugin-react-currency-converter:dev-master
+```
+
+## Configuration
+
+```php
+return [
+    'plugins' => [
+        new \Phergie\Irc\Plugin\React\Command\Plugin(['prefix' => '!']), // dependency
+        new \SimoHeinonen\Phergie\Plugin\CurrencyConverter\Plugin(new \Scheb\YahooFinanceApi\ApiClientFactory::createApiClient()),
+    ]
+];
+```
---
+++ src/Plugin.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace SimoHeinonen\Phergie\Plugin\CurrencyConverter;
+
+use Phergie\Irc\Bot\React\AbstractPlugin;
+use Phergie\Irc\Bot\React\EventQueue;
+use Phergie\Irc\Event\Event;
+use Phergie\Irc\Event\UserEvent;
+use Scheb\YahooFinanceApi\ApiClient;
+
+class Plugin extends AbstractPlugin
+{
+    private $yahooFinance;
+
+    public function __construct(ApiClient $yahooFinance)
+    {
+        $this->yahooFinance = $yahooFinance;
+    }
+
+    public function getSubscribedEvents()
+    {
+        return ['command.currency' => 'convertCurrency'];
+    }
+
+    public function calculate(Event $event, EventQueue $queue)
+    {
+        $params = $event->getCustomParams();
+
+        if (!isset($params[0], $params[1])) {
+            return;
+        }
+
+        try {
+            $exchangeRate = $this->yahooFinance->getExchangeRate($params[0], $params[1]);
+            $msg = $exchangeRate->getName() . ': ' . $exchangeRate->getRate() .PHP_EOL;
+        } catch (\Exception $exception) {
+            $msg = 'Error';
+        }
+
+        $channel = $event->getSource();
+        $queue->ircPrivmsg($channel, $msg);
+    }
+}
+
