1b6ecaddb1c3582710023d057dc4c46a5cc0ebed
Simo Heinonen <simo@dealdash.com>
2021-02-08 17:28:53 +0200

    AA

--- src/Plugin.php
+++ src/Plugin.php
@@ -31,12 +31,89 @@ class Plugin extends AbstractPlugin
 
         try {
             $quote = $this->yahooFinance->getQuote($params[0]);
-            $msg = $quote->getSymbol() . ': ' . $quote->getRegularMarketPrice() .PHP_EOL;
+            if (!$quote) {
+                return;
+            }
+
+            $currencyMap = [
+                'EUR' => '€',
+                'USD' => '$',
+            ];
+
+            $msg = sprintf(
+                '%s (%s)',
+                $quote->getShortName(),
+                $quote->getSymbol()
+            );
+
+            $vari = $quote->getRegularMarketChange() < 0 ? '4' : '3';
+            $regularMarketMsg = sprintf(
+                ' | %s%s%s %s%s (%s%%)%s vol: %s',
+                chr(0x02),
+                ($currencyMap[$quote->getCurrency()] ?? '') . round($quote->getRegularMarketPrice(), 3),
+                chr(0x02),
+                (chr(0x03). $vari),
+                $this->formatNum(round($quote->getRegularMarketChange(), 3)),
+                round($quote->getRegularMarketChangePercent(), 3),
+                chr(0x03),
+                $this->fancyNumber($quote->getRegularMarketVolume())
+            );
+
+            $preMarketMsg = '';
+            if ($quote->getPreMarketPrice()) {
+                $vari = $quote->getPreMarketPrice() < 0 ? '4' : '3';
+                $preMarketMsg = sprintf(
+                    ' | Before hours: %s %s%s (%s%%)%s',
+                    ($currencyMap[$quote->getCurrency()] ?? '') . round($quote->getPreMarketPrice(), 3),
+                    (chr(0x03). $vari),
+                    $this->formatNum(round($quote->getPreMarketChange(), 3)),
+                    round($quote->getPreMarketChangePercent(), 3),
+                    chr(0x03)
+                );
+            }
+
+            $afterMarketMsg = '';
+            if ($quote->getPostMarketPrice()) {
+                $vari = $quote->getPostMarketPrice() < 0 ? '4' : '3';
+                $afterMarketMsg = sprintf(
+                    ' | After hours: %s %s%s (%s%%)%s',
+                    ($currencyMap[$quote->getCurrency()] ?? '') . round($quote->getPostMarketPrice(), 3),
+                    (chr(0x03). $vari),
+                    $this->formatNum(round($quote->getPostMarketChange(), 3)),
+                    round($quote->getPostMarketChangePercent(), 3),
+                    chr(0x03)
+                );
+            }
+
+            $mesegsg = $msg . $regularMarketMsg . $preMarketMsg . $afterMarketMsg. PHP_EOL;
         } catch (\Exception $exception) {
             return;
         }
 
         $channel = $event->getSource();
-        $queue->ircPrivmsg($channel, $msg);
+        $queue->ircPrivmsg($channel, $mesegsg);
     }
+
+    private function fancyNumber($n){
+        if ($n < 1000000) {
+            // Anything less than a million
+            $n_format = number_format($n);
+        } else if ($n < 1000000000) {
+            // Anything less than a billion
+            $n_format = number_format($n / 1000000, 3) . 'M';
+        } else {
+            // At least a billion
+            $n_format = number_format($n / 1000000000, 3) . 'B';
+        }
+        return $n_format;
+    }
+
+    private function formatNum($num){
+        if ($num > 0) {
+            return '+'.$num;
+        }
+
+        return $num;
+    }
+
 }
